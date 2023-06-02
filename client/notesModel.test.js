949287898be2fd0a2c946307036183ec3512aac1
George Barrett <georgeaustinbarrett@gmail.com>
2023-06-02 12:25:08 +0100

    syntax error amendment. also, test for adding a single note. test running GREEN

--- client/notesModel.test.js
+++ client/notesModel.test.js
@@ -2,19 +2,27 @@ const Notes = require('./notesModel');
 
 describe('NotesModel', () => {
   it('constructs with an empty array', () => {
-    notes = new Notes
+    const notes = new Notes();
     expect(notes.getNotes()).toEqual([])
   });
 
+  it('adds a new note to the list', () => {
+    const notes = new Notes();
+
+    notes.addNote('Go to the park')
+    
+    expect(notes.getNotes()).toEqual(['Go to the park'])
+  });
+
   it('should add notes to the model', () => {
-    notes = new Notes
+    const notes = new Notes();
     notes.addNote('Make a cup of tea');
     notes.addNote('Make another cup of tea');
     expect(notes.getNotes()).toEqual(['Make a cup of tea', 'Make another cup of tea'])
   });
 
   it('should reset note list', () => {
-    notes = new Notes
+    const notes = new Notes();
     notes.addNote('Do something');
     notes.addNote('Do more things');
     notes.reset();
